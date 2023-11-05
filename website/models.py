2acbd77cde06cff728575e2809d34db774cd7d74
Fredrik Angelsen <fredrikangelsen@gmail.com>
2023-11-05 16:44:09 +0100

    Inlcuded new database models needed for projects
    Implemented new templates and views for working with projects.

--- website/models.py
+++ website/models.py
@@ -16,3 +16,20 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+
+class Load(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    value = db.Column(db.Float)
+    duty_cycle_id = db.Column(db.Integer, db.ForeignKey('duty_cycle.id'))
+
+class DutyCycle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    loads = db.relationship('Load', cascade="all, delete-orphan")
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    duty_cycles = db.relationship('DutyCycle')
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, Project, DutyCycle, Load
 from . import db
 import json
 
@@ -35,3 +35,123 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
+
+@views.route('/projects', methods=['GET', 'POST'])
+@login_required
+def projects():
+    if request.method == 'POST':
+        project_name = request.form.get('project_name')
+        if project_name:
+            new_project = Project(name=project_name)
+            db.session.add(new_project)
+            db.session.commit()
+            flash('Project created successfully!', 'success')
+            return redirect(url_for('views.projects'))
+        else:
+            flash('Project name is required!', 'error')
+
+    all_projects = Project.query.all()
+    return render_template('projects.html', user=current_user, projects=all_projects)
+
+@views.route('/create_project', methods=['GET', 'POST'])
+@login_required
+def create_project():
+    if request.method == 'POST':
+        project_name = request.form.get('project_name')
+        if not project_name:
+            flash('Project name is required!', category='error')
+        else:
+            new_project = Project(name=project_name)
+            db.session.add(new_project)
+            db.session.commit()
+            flash('Project created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template('create_project.html', user=current_user)
+
+@views.route('/project/<int:project_id>', methods=['GET', 'POST'])
+@login_required
+def project_detail(project_id):
+    project = Project.query.get_or_404(project_id)
+    if request.method == 'POST':
+        duty_cycle_name = request.form.get('duty_cycle_name')
+        if duty_cycle_name:
+            new_duty_cycle = DutyCycle(name=duty_cycle_name, project_id=project.id)
+            db.session.add(new_duty_cycle)
+            db.session.commit()
+            flash('Duty cycle added successfully!', 'success')
+        else:
+            flash('Duty cycle name is required!', 'error')
+    return render_template('project_detail.html', user=current_user, project=project)
+
+@views.route('/edit_duty_cycle/<int:duty_cycle_id>', methods=['GET', 'POST'])
+@login_required
+def edit_duty_cycle(duty_cycle_id):
+    duty_cycle = DutyCycle.query.get_or_404(duty_cycle_id)
+    # Handle the POST request if you want to allow editing duty cycle details
+    return render_template('duty_cycle.html', user=current_user, duty_cycle=duty_cycle)
+
+@views.route('/delete_duty_cycle/<int:duty_cycle_id>', methods=['POST'])
+@login_required
+def delete_duty_cycle(duty_cycle_id):
+    duty_cycle = DutyCycle.query.get_or_404(duty_cycle_id)
+    if duty_cycle:
+        db.session.delete(duty_cycle)
+        db.session.commit()
+        flash('Duty cycle deleted!', 'success')
+    else:
+        flash('You do not have permission to delete this duty cycle.', 'error')
+    return redirect(url_for('views.project_detail', user=current_user, project_id=duty_cycle.project_id))
+
+@views.route('/add_duty_cycle/<int:project_id>', methods=['POST'])
+@login_required
+def add_duty_cycle(project_id):
+    project = Project.query.get_or_404(project_id)
+    duty_cycle_name = request.form.get('duty_cycle_name')
+    if duty_cycle_name:
+        new_duty_cycle = DutyCycle(name=duty_cycle_name, project_id=project.id)
+        db.session.add(new_duty_cycle)
+        db.session.commit()
+        flash('Duty cycle added successfully!', 'success')
+    else:
+        flash('Duty cycle name is required!', 'error')
+    return redirect(url_for('views.project_detail', project_id=project.id))
+
+@views.route('/duty_cycle/<int:duty_cycle_id>', methods=['GET', 'POST'])
+@login_required
+def duty_cycle(duty_cycle_id):
+    duty_cycle = DutyCycle.query.get_or_404(duty_cycle_id)
+    if request.method == 'POST':
+        load_name = request.form.get('load_name')
+        load_value = request.form.get('load_value')
+
+        if not load_name or not load_value:
+            flash('Load name and value are required!', category='error')
+        else:
+            try:
+                load_value = float(load_value)  # Convert the string to a float
+                new_load = Load(name=load_name, value=load_value, duty_cycle_id=duty_cycle_id)
+                db.session.add(new_load)
+                db.session.commit()
+                flash('Load added!', category='success')
+            except ValueError:
+                flash('Please enter a valid number for the load value.', category='error')
+
+    # Retrieve the updated duty cycle to include new loads
+    duty_cycle = DutyCycle.query.get_or_404(duty_cycle_id)
+    return render_template('duty_cycle.html', user=current_user, duty_cycle=duty_cycle)
+
+@views.route('/delete_load/<int:load_id>', methods=['POST'])
+@login_required
+def delete_load(load_id):
+    load = Load.query.get_or_404(load_id)
+    if load:
+        duty_cycle_id = load.duty_cycle_id  # Save duty_cycle_id before deleting the load
+        db.session.delete(load)
+        db.session.commit()
+        flash('Load deleted!', category='success')
+        return redirect(url_for('views.duty_cycle', duty_cycle_id=duty_cycle_id))
+    else:
+        flash('Load not found or you do not have permission to delete it.', category='error')
+        # Redirect to a default page if no load found or not authorized
+        return redirect(url_for('views.projects'))
